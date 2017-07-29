e9aa46c0c51b59ee1a64e83e3520ac5e02402694
Mengting Fang <mtfang@stanford.edu>
2017-07-28 20:12:34 -0700

    modify batchsize again

--- DRAMcopy13_rewrite_filterbank.py
+++ DRAMcopy13_rewrite_filterbank.py
@@ -88,17 +88,17 @@ def linear(x,output_dim):
 
 
 def filterbank(gx, gy, sigma2, delta, N):
-    grid_i = tf.reshape(tf.cast(tf.range(N), tf.float32), [1, -1])
+    # grid_i = tf.reshape(tf.cast(tf.range(N), tf.float32), [1, -1])
     # mu_x = gx + (grid_i - N / 2 - 0.5) * delta # eq 19
     # mu_y = gy + (grid_i - N / 2 - 0.5) * delta # eq 20
    
-    mu_x = [gx[0][0] - tf.reduce_sum((delta[0])[0:14])]
+    mu_x = gx - tf.reduce_sum(delta[0][0:14])]
     for i in range(1,14):
-        mu_xx = tf.reshape(gx[0][0] - tf.reduce_sum((delta[0])[i:14]), [-1])
-        mu_x = tf.concat([mu_x, mu_xx], 0)
+        mu_xx = gx - tf.reduce_sum(delta[0][i:14])
+        mu_x = tf.concat([mu_x, mu_xx], 1)
     for i in range(14,25):
-        mu_xx = tf.reshape(gx[0] + tf.reduce_sum((delta[0])[14:i+1]), [-1])
-        mu_x = tf.concat([mu_x, mu_xx], 0)
+        mu_xx = gx + tf.reduce_sum(delta[0][14:i+1])
+        mu_x = tf.concat([mu_x, mu_xx], 1)
     
     mu_y = mu_x
     
@@ -108,11 +108,8 @@ def filterbank(gx, gy, sigma2, delta, N):
     mu_x = tf.reshape(mu_x, [-1, N, 1])
     mu_y = tf.reshape(mu_y, [-1, N, 1])
     # sigma2 = tf.reshape(sigma2, [-1, 1, 1])
-    ssigma2 = tf.reshape(sigma2[0], [-1,1])
-    Fx = tf.exp(-tf.square((a - mu_x) / (2*ssigma2))) # 2*sigma2?
-    Fy = tf.exp(-tf.square((b - mu_y) / (2*ssigma2))) # batch_size x N x B
-    Fx = tf.reshape([Fx]*batch_size, [batch_size, N, -1])
-    Fy = tf.reshape([Fy]*batch_size, [batch_size, N, -1])
+    Fx = tf.exp(-tf.square((a - mu_x) / (2*tf.transpose(sigma2)))) # 2*sigma2?
+    Fy = tf.exp(-tf.square((b - mu_y) / (2*tf.transpose(sigma2))))) # batch_size x N x B
     # normalize, sum over A and B dims
     Fx=Fx/tf.maximum(tf.reduce_sum(Fx,2,keep_dims=True),eps)
     Fy=Fy/tf.maximum(tf.reduce_sum(Fy,2,keep_dims=True),eps)
@@ -158,9 +155,9 @@ def attn_window(scope,h_dec,N, glimpse):
     delta=tdelta*tf.exp(log_delta[0])
     
     sigma2=delta*delta/4 # sigma=delta/2
-    sigma2=sigma2+0.001*tf.reduce_min(sigma2[0,0:12])
-    delta=[delta] * batch_size
-    sigma2=[sigma2] * batch_size
+    sigma2=sigma2+0.0001*tf.reduce_min(sigma2[0,0:12])
+    # delta=[delta] * batch_size
+    # sigma2=[sigma2] * batch_size
     delta_list[glimpse] = delta
     sigma_list[glimpse] = sigma2
 
