fee7dd54395a27d886bcc16a99bb1a83dff06d88
pandamt <121mengting@163.com>
2017-07-30 15:19:31 -0700

    updata

--- model_settings.py
+++ model_settings.py
@@ -4,7 +4,7 @@ min_blobs = 1
 max_blobs = 9
 
 learning_rate = 1e-3
-glimpses = 11
-batch_size = 77
+glimpses = 25
+batch_size = 100
 
-model_name = "test_linear_split"
+model_name = "test_rewrite_filterbank_006"
