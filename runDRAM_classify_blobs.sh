cafd8ef97def167ec80946d77abcd08eea356223
Toad22222 <Toad22222@users.noreply.github.com>
2017-08-08 16:07:15 -0700

    wrote shell script to run DRAM_classify_blobs

--- runDRAM_classify_blobs.sh
+++ runDRAM_classify_blobs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 for i in {1..10}
 do
-    mkdir "model_runs/DRAM_classify_blobs_run_${i}"
-    nohup python3 -u DRAM_classify_blobs.py > "model_runs/DRAM_classify_blobs_run_${i}/nohup.out&"
+    sed -i '$d' model_settings.py
+    MODELNAME="DRAM_classify_blobs_run_${i}"
+    echo "model_name = '$MODELNAME'" >> model_settings.py
+
+    #mkdir "model_runs/$MODELNAME"
+    #nohup python3 -u DRAM_classify_blobs.py > "model_runs/$MODELNAME/nohup.out&"
 done
 
 ls -lah  
