216a727d3342b0141d2e15b73f2136c45a3e5b04
Toad22222 <Toad22222@users.noreply.github.com>
2017-08-11 15:28:47 -0700

    save choice and confidence data and graph msdcv

--- runDRAM_classify_blobs.sh
+++ runDRAM_classify_blobs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#sed -i '/min_edge/c\min_edge = 2' model_settings.py
+#sed -i '/max_edge/c\max_edge = 5' model_settings.py
+#sed -i '/min_blobs/c\min_blobs = 1' model_settings.py
+#sed -i '/max_blobs/c\max_blobs = 9' model_settings.py
+
 for i in {1..10}
 do
     MODELNAME="DRAM_classify_blobs_run_${i}"
@@ -9,5 +15,3 @@ done
 
 SOMEVAR='done running ^_^'  
 echo "$SOMEVAR"
-
-
