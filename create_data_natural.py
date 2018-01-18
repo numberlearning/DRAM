3d35e47700c551530edb9030ac3dd721c00828b4
Sharon <sychen23@yahoo.com>
2018-01-17 20:43:19 -0800

    update create_data_natural with Mengting's edits

--- create_data_natural.py
+++ create_data_natural.py
@@ -1,17 +1,15 @@
-# MT: modifications by Mengting
-
 import sys
 import numpy as np
 import random
 from model_settings import img_height, img_width, min_edge, max_edge, min_blobs_train, max_blobs_train, min_blobs_test, max_blobs_test # MT
 
+
 def get_S(max_blobs):
     """Get the denominator of proportion of blobs with a specific number of blobs."""
 
-    if S is None:
-        S = 0.0
-        for i in range(1, max_blobs):
-            S += i ** (-2)
+    S = 0.0
+    for i in range(1, max_blobs):
+        S += i ** (-2)
     return S
 
 
@@ -21,7 +19,7 @@ def get_size(testing, num_blobs, max_blobs):
     if testing:
         return 1000 # even: make 1000 images for each number
     else:
-        return int(10000/(num_blobs**(-2))/get_S(max_blobs)) # uneven distribution
+        return int(10000*(num_blobs**(-2))/get_S(max_blobs)) # uneven distribution
 
 
 def get_total(testing, min_blobs, max_blobs): # MT
@@ -39,9 +37,9 @@ def get_k(testing=False):
     """Get the constant scale factor for an individual blob.""" 
     
     if testing:
-        return 3
+        return 3 # testing: k is set to 3 throughout
     else:
-        return random.randint(2, 4)
+        return random.randint(2, 4) # training: k is chosen from the uniform distribution of discrete values 2, 3, and 4
 
 
 def get_s(testing=False, n=None):
@@ -49,26 +47,26 @@ def get_s(testing=False, n=None):
 
     if testing:
         if n is not None:
-            return random.uniform(.8*3 * n ** (-.5), 1.2*3 * n ** (-.5))
+            return random.uniform(.8*3 * (n ** (-.5)), 1.2*3 * (n ** (-.5))) # first 500 displays
         else:
-            return random.uniform(.8, 1.2)
+            return random.uniform(.8, 1.2) # latter 500 displays
     else:
-        return random.uniform(.8*2 * n ** (-.3154), 1.2*2 * n ** (-.3154))
+        return random.uniform(.8*2 * (n ** (-.3154)), 1.2*2 * (n ** (-.3154)))
 
 
 def pir(x):
     """Perform probabilistic integer rounding"""
 
-    f, rem = divmod(x, 1.0)
-    retval = f
+    f, rem = divmod(x, 1)
+    retval = int(f)
     
     if np.random.uniform() < rem:
-        retval = retval + 1.0
+        retval = retval + 1
 
     return retval
 
 
-def get_dims(testing, i):
+def get_dims(testing, i, num_blobs):
     """Get the dimensions of the ith blob."""
 
     k1 = get_k(testing)
@@ -79,15 +77,15 @@ def get_dims(testing, i):
 
     if testing and i >= 500:
         s = get_s(testing)
-    else:
+    elif testing and i < 500: # equal total area
         s = get_s(testing, num_blobs)
+    elif not testing:
+        s = get_s(False, num_blobs)
 
     width = pir(k1 * s)
     height = pir(k2 * s)
 
     return width, height
-
-
 
 def generate_data(testing, min_blobs, max_blobs): # MT
     n_labels = max_blobs_train - min_blobs_train + 1
@@ -104,7 +102,7 @@ def generate_data(testing, min_blobs, max_blobs): # MT
     while (num_blobs < max_blobs + 1):
 
         nOfItem = get_size(testing, num_blobs, max_blobs) # testing: 1000; training, 10000*num_blobs**(-2)/sum(i**(-2))
-        i = 0 # number of images for each blob number
+        i = 0 # amount of images for each blob number
         #sum_edge = 0.0
         #count_edge = 0.0
 
@@ -113,9 +111,9 @@ def generate_data(testing, min_blobs, max_blobs): # MT
             num_count = 0 # amount of blobs in each image 
             used = np.zeros((num_blobs, 4)) # check overlapping
             #total_area = 0.0
-            
+
             while num_count < num_blobs:
-                width, height = get_dims(testing, i)
+                width, height = get_dims(testing, i, num_blobs)
 
                 cX = random.randint(1, 99-width) # top left corner
                 cY = random.randint(1, 99-height)
@@ -140,7 +138,6 @@ def generate_data(testing, min_blobs, max_blobs): # MT
                 #count_edge += 1.0
                 #sum_edge += height
                 #count_edge += 1.0
-
                 for p in range(cY, cY+height):
                     for q in range(cX, cX+width):
                         img[p*img_width+q] = 255
