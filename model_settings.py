1251442c089d308eab2e7d92fccde83f3be07ab0
pandamt <121mengting@163.com>
2018-05-30 09:25:03 -0700

    add response option

--- model_settings.py
+++ model_settings.py
@@ -1,14 +1,14 @@
-img_height = 100
-img_width = 100
+img_height = 50#100
+img_width = 150#100
 min_edge = 4
 max_edge = 6
 p_size = 2
 min_blobs_train = 1
-max_blobs_train = 9
+max_blobs_train = 15#9
 min_blobs_test = 1
-max_blobs_test = 9
+max_blobs_test = 15#9
 test_trials = 500
 
 learning_rate = 1e-2
-glimpses = 10 # 10 run prep, glimpses+1
+glimpses = max_blobs_train + 1 
 batch_size = 1
