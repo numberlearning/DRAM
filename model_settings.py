d2a3ea7b2cfa58c42839d9c44a48cd4bf2b94e78
pandamt <121mengting@163.com>
2018-09-16 13:28:38 -0700

    update

--- model_settings.py
+++ model_settings.py
@@ -1,13 +1,13 @@
-img_height = 50#100
-img_width = 150#100
+img_height = 50
+img_width = 150
 min_edge = 4
 max_edge = 6
 p_size = 2
 min_blobs_train = 1
-max_blobs_train = 15#9
-min_blobs_test = 1
-max_blobs_test = 15#9
-test_trials = 500
+max_blobs_train = 15
+min_blobs_test = 10
+max_blobs_test = 10
+test_trials = 100
 
 learning_rate = 1e-2
 glimpses = max_blobs_train + 1 
