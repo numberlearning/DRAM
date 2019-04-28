1aa091b4479c6e59fd01f441ef3deb343edecbc3
Toad22222 <Toad22222@users.noreply.github.com>
2019-04-27 17:08:24 -0700

    copy over checkpoints of 5 layer models

--- copyDRAM_classify_blobs_ckpts.sh
+++ copyDRAM_classify_blobs_ckpts.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 for i in {1..10}
 do
-    MODELNAME="DRAM_classify_blobs_2_run_${i}"
-    rsync -rP -e ssh "model_runs/$MODELNAME/" "sychen23@psy-pdp-guppy:DRAM/model_runs/$MODELNAME/"
+    MODELNAME="DAA_decrs_fN_5layer_run${i}"
+    echo $MODELNAME
+    rsync -rP -e ssh "../../mtfang/DRAM/model_runs/$MODELNAME/" "sychen23@psy-pdp-guppy:DRAM/model_runs/$MODELNAME/"
 done
 
 SOMEVAR='done running ^_^'  
